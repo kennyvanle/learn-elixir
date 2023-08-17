8438f8662e9e4a455b75f37e651ebd4b837021d6
Kenny <kennyvanle@gmail.com>
2023-08-16 21:15:03 -0400

    string and pipes

---
+++ exercism/high_school_sweetheart.ex
@@ -0,0 +1,35 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.first()
+  end
+
+  def initial(name) do
+    letter = first_letter(name) |> String.upcase()
+    letter <> "."
+  end
+
+  def initials(full_name) do
+    name = String.split(full_name, " ")
+    Enum.map(name, fn word -> initial(word) end) |> Enum.join(" ")
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
+
