78febee176ba0d3347fb7eca00d52408810bcc6a
Kenny <kennyvanle@gmail.com>
2023-08-20 12:38:52 -0400

    win or lost game logic

--- elixir-for-programmers/hangman/hangman/lib/impl/game.ex
+++ elixir-for-programmers/hangman/hangman/lib/impl/game.ex
@@ -27,4 +27,19 @@ defmodule Hangman.Impl.Game do
       letters: word |> String.codepoints()
     }
   end
+
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game = %{ game_state: state }, _guess) 
+  when state in [:won, :lost] do
+    { game, tally(game) }
+  end
+
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list() |> Enum.sort
+    }
+  end
 end
--- elixir-for-programmers/hangman/hangman/test/impl_game_test.exs
+++ elixir-for-programmers/hangman/hangman/test/impl_game_test.exs
@@ -27,5 +27,14 @@ defmodule HangmanImplGameTest do
     end)
   end
 
+  test "state doesn't change if a game is won or lost" do
+    for state <- [:won, :lost] do 
+      game = Game.new_game("wombat")
+      game = Map.put(game, :game_state, state)
+      { new_game, _tally } = Game.make_move(game, "x")
+      assert new_game == game
+    end
+  end
+
 end
 
