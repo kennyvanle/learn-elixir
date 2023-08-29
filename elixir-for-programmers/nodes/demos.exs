29ffa4548b6c4ea5539a3454caa479fcb0a4009d
Kenny <kennyvanle@gmail.com>
2023-08-29 18:59:20 -0400

    nodes and distributed elixir

---
+++ elixir-for-programmers/nodes/demos.exs
@@ -0,0 +1,11 @@
+defmodule Demo do
+  
+  def reverse do
+    receive do
+      { from_pid, msg } ->
+        result = msg |> String.reverse
+        send from_pid, result
+        reverse()
+    end
+  end
+end
