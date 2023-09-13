3c69777a57ee9c473e3adf76d536d82d7c4a46ff
Kenny <kennyvanle@gmail.com>
2023-09-12 21:48:55 -0400

    add watchdog process

--- elixir-for-programmers/hangman/hangman/lib/runtime/server.ex
+++ elixir-for-programmers/hangman/hangman/lib/runtime/server.ex
@@ -1,8 +1,13 @@
 defmodule Hangman.Runtime.Server do
 
+  alias Hangman.Runtime.Watchdog
+
   @type t :: pid()
 
+  @idle_timeout 1 * 60 * 60 * 1000  # 1 hour
+
   alias Hangman.Impl.Game
+
   use GenServer
 
   # client process
@@ -14,15 +19,17 @@ defmodule Hangman.Runtime.Server do
   # server process
 
   def init(_) do
-    { :ok, Game.new_game }
+    watcher = Watchdog.start(@idle_timeout)
+    { :ok, { Game.new_game, watcher }}
   end
 
-  def handle_call({ :make_move, guess }, _from, game) do
+  def handle_call({ :make_move, guess }, _from, { game, watcher }) do
+    Watchdog.im_alive(watcher)
     { updated_game, tally } = Game.make_move(game, guess)
-    { :reply, tally, updated_game }
+    { :reply, tally, { updated_game, watcher }}
   end
 
-  def handle_call({ :tally }, _from, game) do
-    { :reply, Game.tally(game), game }
+  def handle_call({ :tally }, _from, { game, watcher}) do
+    { :reply, Game.tally(game), { game, watcher}}
   end
 end
