a8af734d5f2d5082dbd4c44c6148bb6dfb95424e
Kenny <kennyvanle@gmail.com>
2023-09-12 21:19:16 -0400

    refactor, autofocus and button

--- elixir-for-programmers/hangman/b1/lib/b1_web/controllers/hangman_html.ex
+++ elixir-for-programmers/hangman/b1/lib/b1_web/controllers/hangman_html.ex
@@ -4,13 +4,13 @@ defmodule B1Web.HangmanHTML do
   embed_templates("hangman_html/*")
 
   def continue_or_try_again(conn, status) when status in[ :won, :lost ] do
-    Phoenix.HTML.Link.button("Try again", to: B1Web.Router.Helpers.hangman_path(conn, :new))
+    Phoenix.HTML.Link.button("Try again", to: B1Web.Router.Helpers.hangman_path(conn, :new), class: "py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700")
   end
 
   def continue_or_try_again(conn, _) do
     Phoenix.HTML.Form.form_for(conn, B1Web.Router.Helpers.hangman_path(conn,  :update), [ as: ":make_move", method: :put ], fn f ->
       [
-        Phoenix.HTML.Form.text_input(f, :guess),
+        Phoenix.HTML.Form.text_input(f, :guess, autofocus: "true"),
         " ",
         Phoenix.HTML.Form.submit("Make guess", class: "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800")
       ]
